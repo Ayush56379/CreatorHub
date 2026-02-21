5c4a1f63723d7d3430730dc6c769797a36c65312
Ayush Prajapati <ayushprajpati6@gmail.com>
2026-02-21 08:54:53 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,114 +1,38 @@
 import express from "express";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import User from "../models/User.js";
 
-const router = express.Router();
+import {
 
-
-// REGISTER
-
-router.post("/register", async (req,res)=>{
-
-try{
-
-const {name,email,password} = req.body;
-
-const existUser = await User.findOne({email});
-
-if(existUser){
-
-return res.json({
-message:"User already exists"
-});
+registerUser,
+loginUser
 
 }
 
-const hashedPassword =
-await bcrypt.hash(password,10);
+from "../controllers/authController.js";
 
-const user = await User.create({
-
-name,
-email,
-password:hashedPassword
-
-});
-
-res.json({
-
-message:"User Registered",
-user
-
-});
-
-}catch(error){
-
-res.status(500).json(error.message);
-
-}
-
-});
+const router =
+express.Router();
 
 
+// Register
 
-// LOGIN
+router.post(
 
-router.post("/login", async(req,res)=>{
+"/register",
 
-try{
-
-const {email,password} = req.body;
-
-const user = await User.findOne({email});
-
-if(!user){
-
-return res.json({
-message:"User not found"
-});
-
-}
-
-const match =
-await bcrypt.compare(
-password,
-user.password
-);
-
-if(!match){
-
-return res.json({
-message:"Wrong password"
-});
-
-}
-
-const token = jwt.sign(
-
-{id:user._id},
-
-process.env.JWT_SECRET,
-
-{expiresIn:"7d"}
+registerUser
 
 );
 
-res.json({
 
-message:"Login Success",
+// Login
 
-token,
-user
+router.post(
 
-});
+"/login",
 
-}catch(error){
+loginUser
 
-res.status(500).json(error.message);
+);
 
-}
-
-});
 
 export default router;
