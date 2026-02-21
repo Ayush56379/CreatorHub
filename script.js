e4c4a4a3b0a949c1c6874e5cc1799efca6457955
Ayush Prajapati <ayushprajpati6@gmail.com>
2026-02-21 14:58:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("CreatorHub Ready");
+async function handleRegister(){
+
+const name =
+document.getElementById("name").value;
+
+const email =
+document.getElementById("email").value;
+
+const password =
+document.getElementById("password").value;
+
+const response =
+await registerUser({
+
+name,
+email,
+password
+
+});
+
+alert(response.message);
+
+}
