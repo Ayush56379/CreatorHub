ced73c2aa42df636d6a090f380215d500eb70f7b
Ayush Prajapati <ayushprajpati6@gmail.com>
2026-02-21 09:02:01 +0530

    Update dashboardRoutes.js

--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -6,7 +6,48 @@ import User from "../models/User.js";
 import authMiddleware from
 "../middleware/authMiddleware.js";
 
-const router = express.Router();
+const router =
+express.Router();
+
+
+// ================= MY PROFILE =================
+
+router.get(
+
+"/profile",
+
+authMiddleware,
+
+async(req,res)=>{
+
+try{
+
+const user =
+await User.findById(
+
+req.user.id
+
+).select("-password");
+
+res.json({
+
+success:true,
+user
+
+});
+
+}catch{
+
+res.status(500).json({
+
+message:"Profile Error"
+
+});
+
+}
+
+});
+
 
 
 // ================= CREATOR PRODUCTS =================
@@ -39,7 +80,7 @@ products
 
 res.status(500).json({
 
-message:"Error"
+message:"Fetch Error"
 
 });
 
@@ -47,75 +88,6 @@ message:"Error"
 
 });
 
-
-// ================= ADMIN ALL USERS =================
-
-router.get(
-
-"/admin/users",
-
-authMiddleware,
-
-async(req,res)=>{
-
-try{
-
-const users =
-await User.find();
-
-res.json({
-
-success:true,
-users
-
-});
-
-}catch{
-
-res.status(500).json({
-
-message:"Error"
-
-});
-
-}
-
-});
-
-
-// ================= ADMIN ALL PRODUCTS =================
-
-router.get(
-
-"/admin/products",
-
-authMiddleware,
-
-async(req,res)=>{
-
-try{
-
-const products =
-await Product.find();
-
-res.json({
-
-success:true,
-products
-
-});
-
-}catch{
-
-res.status(500).json({
-
-message:"Error"
-
-});
-
-}
-
-});
 
 
 export default router;
