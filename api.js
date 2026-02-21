0ed8486cd9eafdeb932654b327d47a8051ba7085
Ayush Prajapati <ayushprajpati6@gmail.com>
2026-02-21 14:51:21 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,60 @@
-const API =
+// CreatorHub API Service
 
-"https://your-backend-url.com";
+const API_URL =
+"https://creatorhub-f2fe.onrender.com/api";
 
-export default API;
+
+// REGISTER
+
+async function registerUser(data){
+
+const res =
+await fetch(API_URL + "/auth/register",{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify(data)
+
+});
+
+return res.json();
+
+}
+
+
+// LOGIN
+
+async function loginUser(data){
+
+const res =
+await fetch(API_URL + "/auth/login",{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify(data)
+
+});
+
+return res.json();
+
+}
+
+
+// PRODUCTS
+
+async function getProducts(){
+
+const res =
+await fetch(API_URL + "/products");
+
+return res.json();
+
+}
