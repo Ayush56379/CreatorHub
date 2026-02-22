e3a83d6f3834f387ee29639746376adfb7038af8
Ayush Prajapati <ayushprajpati6@gmail.com>
2026-02-22 08:19:36 +0530

    Update Order.js

--- server/models/Order.js
+++ server/models/Order.js
@@ -4,28 +4,55 @@ const orderSchema =
 new mongoose.Schema({
 
 user:{
- type:String,
- required:true
+
+type:mongoose.Schema.Types.ObjectId,
+
+ref:"User",
+
+required:true
+
 },
 
 product:{
- type:String,
- required:true
+
+type:mongoose.Schema.Types.ObjectId,
+
+ref:"Product",
+
+required:true
+
 },
 
-paid:{
- type:Boolean,
- default:false
+price:{
+
+type:Number,
+
+required:true
+
 },
 
-createdAt:{
- type:Date,
- default:Date.now
+status:{
+
+type:String,
+
+default:"paid"
+
 }
 
-});
+},
+
+{
+
+timestamps:true
+
+}
+
+);
 
 export default mongoose.model(
+
 "Order",
+
 orderSchema
+
 );
