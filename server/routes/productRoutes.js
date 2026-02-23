2a843230e5104f54ff9f06f81605f5bf8c8b9632
Ayush Prajapati <ayushprajpati6@gmail.com>
2026-02-23 09:01:45 +0530

    Update productRoutes.js

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,90 +1,8 @@
-import express from "express";
-import Product from "../models/Product.js";
-import Order from "../models/Order.js";
-import authMiddleware from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-
-
-// ================= GET ALL PRODUCTS =================
-
-router.get("/", async(req,res)=>{
-
-try{
-
-const products = await Product.find()
-.sort({createdAt:-1});
-
-res.json({
-
-success:true,
-products
-
-});
-
-}catch{
-
-res.status(500).json({
-
-message:"Fetch Error"
-
-});
-
-}
-
-});
-
-
-// ================= SINGLE PRODUCT =================
-
-router.get("/:id", async(req,res)=>{
-
-try{
-
-const product = await Product.findById(
-
-req.params.id
-
-);
-
-if(!product){
-
-return res.status(404).json({
-
-message:"Product Not Found"
-
-});
-
-}
-
-res.json({
-
-success:true,
-product
-
-});
-
-}catch{
-
-res.status(500).json({
-
-message:"Error"
-
-});
-
-}
-
-});
-
-
-// ================= UPLOAD EBOOK (Drive Link) =================
+// ================= DRIVE LINK UPLOAD =================
 
 router.post(
-
-"/products",
-
+"/",
 authMiddleware,
-
 async(req,res)=>{
 
 try{
@@ -99,8 +17,6 @@ pdf
 } = req.body;
 
 
-// Validation
-
 if(!title || !price || !image || !pdf){
 
 return res.status(400).json({
@@ -112,14 +28,12 @@ message:"Fill All Fields"
 }
 
 
-// Save Product
-
 const product = await Product.create({
 
 title,
 price,
 image,
-pdf,
+fileLink:pdf,
 
 creator:req.user.id
 
@@ -129,7 +43,8 @@ creator:req.user.id
 res.status(201).json({
 
 success:true,
-message:"Ebook Uploaded 🔥",
+message:"Ebook Uploaded Successfully",
+
 product
 
 });
@@ -147,43 +62,3 @@ message:"Upload Failed"
 }
 
 });
-
-
-// ================= DELETE PRODUCT =================
-
-router.delete(
-
-"/:id",
-
-authMiddleware,
-
-async(req,res)=>{
-
-try{
-
-await Product.findByIdAndDelete(
-
-req.params.id
-
-);
-
-res.json({
-
-success:true,
-message:"Deleted"
-
-});
-
-}catch{
-
-res.status(500).json({
-
-message:"Delete Error"
-
-});
-
-}
-
-});
-
-export default router;
